9484fe40e2801b2ecb9ed7cdd0c367f4526d96b6
frankvargashn <ing.frankvargas@gmail.com>
2021-01-25 13:17:56 -0600

    compare-ejemplos

--- js/rutas.js
+++ js/rutas.js
@@ -18,3 +18,18 @@
             });
 
 } //fin funcion
+
+     var animacion;
+        function rotarCamara(timestamp) {
+
+            rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+            map.rotateTo(rotacion, { duration: 0 });
+
+            animacion = requestAnimationFrame(rotarCamara);
+        }
+
+
+        function finalRotarCamara() {
+
+            cancelAnimationFrame(animacion);
+        }   
